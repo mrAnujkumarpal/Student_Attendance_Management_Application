e234d821ab7d9a2d97a350c8f9a7eaf4261219e6
mrAnujkumarpal <mail4uanuj@gmail.com>
2022-09-02 06:49:22 +0530

    Added new ENUMS.

---
+++ src/main/java/com/bit/sams/entity/enums/Enums.java
@@ -0,0 +1,13 @@
+package com.bit.sams.entity.enums;
+
+public class Enums {
+
+    public enum HIGHEST_DEGREE {
+        DIPLOMA,GRADUATE,POST_GRADUATE,DOCTORATE
+    }
+
+    public enum NOTIFICATION_TYPE {
+        EMAIL, SMS
+    }
+    
+}
